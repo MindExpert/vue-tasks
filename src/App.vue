8002964c11d540ebd8d3a5fd2d90783cfb2690ba
e-muchasaj <e.muchasaj@7btc.co>
2025-01-27 09:19:03 +0100

    Added Lyaout and route components and active links

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,15 @@
 <script setup lang="ts">
-import Input from '@/components/ui/input/Input.vue'
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Sidebar from './components/Layout/Sidebar.vue'
+import TopNavbar from './components/Layout/TopNavbar.vue'
 </script>
 
 <template>
-    <nav class="h-16 border-b bg-muted/40 flex items-center justify-between px-6">
-        <form class="relative w-full h-fit max-w-96">
-            <iconify-icon
-                class="absolute top-[50%] translate-y-[-50%] left-2 text-muted-foreground"
-                icon="lucide:search"
-            ></iconify-icon>
-            <Input
-                type="search"
-                placeholder="Search projects"
-                class="w-full pl-8 bg-background h-8 rounded-lg border-primary/60 focus:outline-none"
-            />
-        </form>
+    <Sidebar />
+    <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+        <TopNavbar />
 
-        <div class="flex items-center space-x-4">
-            <router-link class="flex items-center space-x-2" to="/">Home</router-link>
-            <router-link class="flex items-center space-x-2" to="/projects">Projects</router-link>
-            <router-link class="flex items-center space-x-2" to="/tasks">Tasks</router-link>
-        </div>
-
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Avatar>
-                    <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                    <AvatarFallback>EM</AvatarFallback>
-                </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent class="w-56">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem disabled>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscriptions</DropdownMenuItem>
-                <DropdownMenuItem><span>Log out</span> </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    </nav>
-    <main>
-        <RouterView />
-    </main>
+        <main>
+            <RouterView />
+        </main>
+    </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/Layout/SidebarLinks.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface LinkProp {
+    title: string
+    to: string
+    icon: string
+}
+
+defineProps<{
+    links: LinkProp[]
+}>()
+</script>
+
+<template>
+    <RouterLink
+        v-for="(link, index) in links"
+        :key="index"
+        :to="link.to"
+        exact-active-class="bg-muted text-white"
+        class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    >
+        <iconify-icon :icon="link.icon"></iconify-icon>
+        <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+</template>
+
+<!-- <style lang="css" scoped>
+.router-link-active {
+    color: var(--color-primary);
+    background-color: black;
+}
+</style> -->

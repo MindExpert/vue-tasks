609a7c021c10124bf9ab1bc19c39be5405fb34ea
e-muchasaj <e.muchasaj@7btc.co>
2025-02-05 11:41:13 +0100

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-const animate = require('tailwindcss-animate')
-
-// import animate from 'tailwindcss-animate'
+import animate from 'tailwindcss-animate'
+//const animate = require('tailwindcss-animate')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
